6b0cb543a17bea487ab5751426a1e7d9a9e7c2f1
talitasooliveira <talitasouzaengcivil@gmail.com>
2024-03-05 12:16:04 +0000

    Revised comments

--- details.py
+++ details.py
@@ -1,12 +1,12 @@
-#Pratical Task 2
-"""Input user's name
-Input user's age
-Input user's house number
-Input user's street name
-Print a single sentence using the details of the user"""
+# Pratical Task 2
 
+# Input user's name
 name = input("What's your name? ")
+# Input user's age
 age = input("What's your age? ")
+# Input user's house number
 house_number = input("What's your house number? ")
+# Input user's street name
 street_name = input("What's your street name? ")
+#Print a single sentence using the details of the user
 print("This is " + name + ". He/She is " + age + " years old and lives at house number " + house_number + " on " + street_name + ".")
