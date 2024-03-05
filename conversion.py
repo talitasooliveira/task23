4233d028a76e88b8a769653b0b4406b8b69781cb
talitasooliveira <159443145+talitasooliveira@users.noreply.github.com>
2024-03-05 11:36:19 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,23 @@
+#Pratical Task 3
+"""Create 3 types of variables
+Convert one to another one"""
+
+num1 = 99.23    #float
+num2 = 23       #integer
+num3 = "text"   #string **not's possible convert to numbers**
+
+#Convert float to integer
+x = int(num1)
+print(x)
+
+#Convert float to string
+x1 = str(num1)
+print(x1)
+
+#Convert integer to float
+y = float(num2)
+print(y)
+
+#Convert integer to string
+y1 = str(num2)
+print(y1)
---
+++ details.py
@@ -0,0 +1,12 @@
+#Pratical Task 2
+"""Input user's name
+Input user's age
+Input user's house number
+Input user's street name
+Print a single sentence using the details of the user"""
+
+name = input("What's your name? ")
+age = input("What's your age? ")
+house_number = input("What's your house number? ")
+street_name = input("What's your street name? ")
+print("This is " + name + ". He/She is " + age + " years old and lives at house number " + house_number + " on " + street_name + ".")
